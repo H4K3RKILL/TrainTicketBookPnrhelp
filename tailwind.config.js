a76455f855304fd3a4bf1354d7fce9225927be08
Maudnitzsche10001 <maud.nitzsche@go.sfcollege.edu>
2025-06-18 01:45:45 +0530

    feat: Configure and verify Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This scans App.js and any other components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
